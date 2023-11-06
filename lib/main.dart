818a2be805db8ceb35d16bc5683023c3f26c64c4
Reza Rizqi Ramdhani <rezarizqir@gmail.com>
2023-11-06 12:57:31 +0700

    Update: init firebase crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -31,9 +31,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 import 'package:dicoding_ditonton/injection.dart' as di;
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await di.init();
   runApp(MyApp());
 }
